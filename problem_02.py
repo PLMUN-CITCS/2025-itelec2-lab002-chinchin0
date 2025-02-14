617c12214c3a1a9a4b1ab5b055a6e909f6580521
shenarie nicole <MagsayoShenarieNicole_Bsit@plmun.edu.ph>
2025-02-14 14:27:07 +0800

    Submitting Python Session 03 - Exercise 01

--- problem_02.py
+++ problem_02.py
@@ -4,6 +4,7 @@
 # Square the Number Program
 
 def main():
+   
    print("Square the Number Program")
    number = int(input("Enter a number: "))
    print(f"The square of {number} is {(number * number):.2f}")
